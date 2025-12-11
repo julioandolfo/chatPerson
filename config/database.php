1a1e45bbe074c0b50169b29935d3bfea8df778b7
Julio Andolfo <juliosertori@gmail.com>
2025-12-11 10:50:00 -0300

    corr87

--- config/database.php
+++ config/database.php
@@ -6,9 +6,9 @@
 return [
     'host' => $_ENV['DB_HOST'] ?? 'localhost',
     'port' => $_ENV['DB_PORT'] ?? 3306,
-    'database' => $_ENV['DB_DATABASE'] ?? 'chat',
-    'username' => $_ENV['DB_USERNAME'] ?? 'root',
-    'password' => $_ENV['DB_PASSWORD'] ?? '',
+    'database' => $_ENV['DB_DATABASE'] ?? 'chat_person',
+    'username' => $_ENV['DB_USERNAME'] ?? 'chatperson',
+    'password' => $_ENV['DB_PASSWORD'] ?? '@Ative199#Cesar123',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'options' => [
